79597edf73a322b52e5f1f5fb6d34e62436d0b0e
Chris Washington <chris.washington@capitalone.com>
2020-01-06 13:45:16 -0500

    switched to prettier config

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,16 @@
 module.exports = {
   verbose: true,
   moduleFileExtensions: ["js", "jsx", "json", "vue"],
+
   transform: {
     "^.+\\.vue$": "vue-jest",
     ".+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$": "jest-transform-stub",
     "^.+\\.(js|jsx)?$": "babel-jest"
   },
+
   collectCoverage: true,
   collectCoverageFrom: ["src/**/*.js", "!**/node_modules/**"],
+
   coverageThreshold: {
     global: {
       branches: 100,
@@ -16,6 +19,7 @@ module.exports = {
       statements: 100
     }
   },
+
   reporters: [
     "default",
     [
@@ -27,16 +31,22 @@ module.exports = {
       }
     ]
   ],
+
   testEnvironment: "jest-environment-jsdom-fifteen",
   transformIgnorePatterns: ["/node_modules/(?!lodash-es).+\\.js$"],
+
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1"
   },
+
   snapshotSerializers: ["jest-serializer-vue"],
+
   testMatch: [
     "**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)",
     "!**/.stryker-tmp/**/*.spec.(js|jsx|ts|tsx)"
   ],
+
   testURL: "http://localhost/",
-  watchPlugins: ["jest-watch-typeahead/filename", "jest-watch-typeahead/testname"]
+  watchPlugins: ["jest-watch-typeahead/filename", "jest-watch-typeahead/testname"],
+  preset: "@vue/cli-plugin-unit-jest"
 };
