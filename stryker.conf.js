f56800afaccf9f213c06e65de9c52a2996ea22bb
Chris Washington <chris.washington@capitalone.com>
2020-01-06 10:56:10 -0500

    Adding stryker config

---
+++ stryker.conf.js
@@ -0,0 +1,15 @@
+// This config was generated using a preset.
+// Please see the handbook for more information: https://github.com/stryker-mutator/stryker-handbook/blob/master/stryker/guides/vuejs.md#vuejs
+module.exports = function asyncConfig(config) {
+  config.set({
+    mutate: ['src/**/*.js'],
+    mutator: 'vue',
+    testRunner: 'jest',
+    jest: {
+      // eslint-disable-next-line global-require
+      config: require('./jest.config.js'),
+    },
+    reporters: ['progress', 'clear-text', 'html'],
+    coverageAnalysis: 'on',
+  });
+};
