3fdae54eca6179f0b6e0eb3d46e1dc690ef60424
Chris Washington <chris.washington@capitalone.com>
2020-01-06 15:52:21 -0500

    adding favicon and web app support

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = (options, context) => ({
-  title: 'Form',
+  title: 'Vue Form',
   description: 'Simplify form validations and handling with Vue Form',
   base: process.env.NODE_ENV === 'production'
     ? '/chris-washington/vue-form/'
@@ -10,8 +10,21 @@ module.exports = (options, context) => ({
   activeHeaderLinks: true,
   evergreen: true,
   head: [
-    ['link', { rel: 'icon', href: '/favicon.ico' }]
+    ['link', { rel: 'icon', href: '/images/favicon.ico' }],
+    ['link', { rel: 'icon', href: '/images/android-chrome-192x192.png', sizes: '192x192', type: 'image/png' }],
+    ['link', { rel: 'icon', href: '/images/android-chrome-512x512.png', sizes: '512x512', type: 'image/png' }],
+    ['link', { rel: 'shortcut icon', href: '/images/favicon-16x16.png' }],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'white' }],
+    ['link', { rel: 'apple-touch-icon', href: '/images/apple-touch-icon.png' }],
+    ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
   ],
+  plugins: ['@vuepress/pwa', {
+      serviceWorker: true,
+      updatePopup: true
+  }],
   alias: {
     'vue-form': `${process.cwd()}/dist-babel/index.js`
   },
@@ -19,9 +32,6 @@ module.exports = (options, context) => ({
     lineNumbers: true
   },
   themeConfig: {
-    serviceWorker: {
-      updatePopup: true
-    },
     logo: '/images/logo.png',
     search: true,
     searchMaxSuggestions: 10,
